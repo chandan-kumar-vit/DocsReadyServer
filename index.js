4ab8951f4a040aca9e2a38d994433b98a7f5bcb0
Chandan Kumar <thechandankumar18@gmail.com>
2021-12-06 20:18:26 +0530

    Added feature to upload file in base64 format

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const connectToMongo = require('./db')
+const connectToMongo = require('./db');
+const fileupload = require("express-fileupload");
+const bodyParser = require('body-parser')
 
 dotenv.config();
 const app = express();
 connectToMongo();
 app.use(express.json());
+app.use(fileupload());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 const port = process.env.PORT || 5000;
 
 // Available routes
